3ba07d02aae33344d08819b235cc5a2817c9ec1b
agusrichard <agus.richard21@gmail.com>
2021-03-20 20:07:27 +0700

    Differentiate develop and production environment

--- app/config/envs.py
+++ app/config/envs.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 # Setup base directory and load environment variables
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR, 'dev.env'))
 sys.path.append(BASE_DIR)
 
 
